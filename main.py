43e55753ad538ec804815d1ea5681dccb2859909
joonasaarto <saartojoona@gmail.com>
2025-04-30 14:45:44 +0300

    test 2

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def add_password():
     Returns:
         None
     """
-    website = input("Enter the website: ")
+    website = input("Enter website: ")
     username = input("Enter your username: ")
     password = input("Enter your password: ")
 
